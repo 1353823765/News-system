dde256b8d312b883b37327ae4db7fbff5460922d
1353823765@qq.com <1353823765@qq.com>
2022-11-11 22:12:05 +0800

    11-10

--- src/components/Newsandbox/SideMenu.js
+++ src/components/Newsandbox/SideMenu.js
@@ -1,38 +1,33 @@
-import React from 'react'
-import {
+import React, { useState } from "react";
+import "./SideMenu.css";
+import { UserOutlined, VideoCameraOutlined,SettingOutlined  } from "@ant-design/icons";
+import { Layout, Menu } from "antd";
 
-  UploadOutlined,
-  UserOutlined,
-  VideoCameraOutlined,
-} from '@ant-design/icons';
-import { Layout, Menu } from 'antd';
-const { Sider, } = Layout;
+const { Sider } = Layout;
+
+ const { SubMenu } = Menu;
 export default function SideMenu() {
+  const [collapsed, setCollapsed] = useState(false);
   return (
-    <Sider trigger={null} collapsible collapsed>
-    <div className="logo" />
-    <Menu
-      theme="dark"
-      mode="inline"
-      defaultSelectedKeys={['1']}
-      items={[
-        {
-          key: '1',
-          icon: <UserOutlined />,
-          label: 'nav 1',
-        },
-        {
-          key: '2',
-          icon: <VideoCameraOutlined />,
-          label: 'nav 2',
-        },
-        {
-          key: '3',
-          icon: <UploadOutlined />,
-          label: 'nav 3',
-        },
-      ]}
-    />
-  </Sider>
-  )
+    <Sider trigger={null} collapsible collapsed={collapsed}>
+      <div className="logo">全球新闻发布系统</div>
+      <Menu theme="dark" mode="inline" defaultSelectedKeys={["1"]}>
+        <Menu.Item icon={<UserOutlined />} key="1">
+        nav1
+        </Menu.Item>
+        <Menu.Item icon={<VideoCameraOutlined />} key="2">
+          nav2
+        </Menu.Item>
+        <Menu.Item icon={<VideoCameraOutlined />} key="3">
+          nav3
+        </Menu.Item>
+       
+      </Menu>
+    </Sider>
+  );
 }
+// <SubMenu>
+// <Menu.Item icon={<VideoCameraOutlined />} key="3">1</Menu.Item>
+// <Menu.Item icon={<VideoCameraOutlined />} key="3">2</Menu.Item>
+// <Menu.Item icon={<VideoCameraOutlined />} key="3">3</Menu.Item>
+// </SubMenu>
